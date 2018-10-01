6215efa57fc0020a47f1967ae2f6b46ec8811d5c
Tiger Chen <tigerchen@Tigers-MBP.gateway>
2018-10-01 22:34:15 +1000

    updated logging with local time and seperated the info log and error log

--- startup/logging.js
+++ startup/logging.js
@@ -1,14 +1,19 @@
+const moment = require('moment');
 const winston = require('winston');
 require('winston-daily-rotate-file')
 require('express-async-errors');
 
 module.exports = () => {
+    const loggingTimeFormat = 'YYYY-MM-DD HH:mm:ss:SSS ZZ';
     winston.handleExceptions(
         new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.DailyRotateFile({ 
             dirname: './logs/',
             filename: '%DATE%-uncaughtExceptions.log',
-            datePattern: 'DDMMYYYY'
+            datePattern: 'DDMMYYYY',
+            timestamp: function () {
+                return moment().format(loggingTimeFormat);
+            }
         })
     );
 
@@ -17,9 +22,27 @@ module.exports = () => {
     });
 
     winston.add(winston.transports.DailyRotateFile, {
+        name: 'info-log',
         dirname: './logs/',
         filename: '%DATE%-log.log',
         datePattern: 'DDMMYYYY',
-        prepend: true
+        timestamp: function () {
+            return moment().format(loggingTimeFormat);
+        },
+        prepend: true,
+        level: 'info'
+    });
+
+
+    winston.add(winston.transports.DailyRotateFile, {
+        name: 'error-log',
+        dirname: './logs/',
+        filename: '%DATE%-error.log',
+        datePattern: 'DDMMYYYY',
+        timestamp: function () {
+            return moment().format(loggingTimeFormat);
+        },
+        prepend: true,
+        level: 'error'
     });
 }
