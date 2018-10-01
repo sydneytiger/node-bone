cce0a0daaa511c9ee4e3707687e4b3e218974dc9
Tiger Chen <tigerchen@Tigers-MBP.gateway>
2018-10-01 17:36:03 +1000

    added validation setting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/db')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
