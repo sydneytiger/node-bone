69bcd9ae7c9a23dfa9779f7ce61f9271c1cd4871
Tiger Chen <tigerchen@Tigers-MBP.gateway>
2018-10-02 21:33:25 +1000

    implemented login route

---
+++ routes/login.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const winston = require('winston');
+const {User} = require('../models/user');
+const bcrypt = require('bcrypt');
+
+router.post('/', async (req, res) => {
+    const loginModel = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    const {error} = validateLoginModel(loginModel);
+    if(error)
+    {
+        winston.info(error.message, error);
+        res.status(400).send('Invalid login');
+    }
+
+    const user = await User.findOne({email: loginModel.email});
+    if(!user) res.status(400).send('Invalid user')
+    
+
+    const isValidPassword = await bcrypt.compare(loginModel.password, user.password);
+    if(!isValidPassword) res.status(400).send('invalid password');
+
+    const token = user.generateAuthToken();
+
+    res.header('x-auth-token', token).send({result: true});
+    
+});
+
+
+function validateLoginModel(login) {
+    const joiSchema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(login, joiSchema);
+}
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('../routes/users');
+const login = require('../routes/login');
 const error = require('../middleware/error');
 
 module.exports = (app) => {
     app.use(express.json());
     app.use('/api/users', users);
+    app.use('/api/login', login);
     app.use(error);
 }
