53bdba8a075cd08728dd03ef01a1194605f0b44e
Tiger Chen <tigerchen@Tigers-MBP.gateway>
2018-10-18 02:12:49 +1100

    added more unit tests to user validation

--- test/user.test.js
+++ test/user.test.js
@@ -9,8 +9,10 @@ describe('/api/users', () => {
 
     afterEach(async () => {
         await server.close();
+        await User.remove({});
     });
     describe('GET /myself', () => {
+        debugger;
         const url = '/api/users/myself';
         it('should return 401 if no token is provided', async () => {
             const res = await request(server).get(url);
@@ -31,15 +33,21 @@ describe('/api/users', () => {
 
     describe('POST /', () => {
         const url = '/api/users/';
-        it('should return 400 when the user object is invalid', async () => {
-            const res = await request(server)
-                .post(url)
-                .send({firstName: '123'});
-            expect(res.status).toEqual(400);
-            expect(res.text).toEqual('Invalid input');
-        });
+        it('should return 400 if the email is already registered', async () => {
+            const validUser = {
+                firstName: '12345',
+                lastName: '12345',
+                email: '12345@email.com',
+                password: '12345'
+            };
+            const user = new User(validUser);
+            await user.save();
 
-        //it('should return 400 if the email is already registered');
+            const res = await request(server).post(url).send(validUser);
+            expect(res.status).toEqual(400);
+            expect(res.text).toEqual('User already registered');
+
+        });
 
         // it('should encrypt the password');
 
@@ -67,10 +75,76 @@ describe('user model', () => {
         expect(error).toBeNull();
     });
 
+    it('should error when firstName is empty', () => {
+        user.firstName = '';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
     it('should error when firstName is less then 3 characters', () => {
         user.firstName = '12';
         const { error } = validate(user);
-        expect(error).toBeDefined();
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when firstName is less then 50 characters', () => {
+        user.firstName = Array(52).join('1');
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when lastName is empty', () => {
+        user.lastName = '';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when lastName is less then 3 characters', () => {
+        user.lastName = '12';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when lastName is less then 50 characters', () => {
+        user.lastName = Array(52).join('1');
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when email is empty', () => {
+        user.email = '';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when email is less then 255 characters', () => {
+        user.email = Array(256).join('1');
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when email format is invalid', () => {
+        user.email = '111111';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when password is empty', () => {
+        user.password = '';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when password is less then 5 characters', () => {
+        user.password = '1234';
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
+    });
+
+    it('should error when password is less then 255 characters', () => {
+        user.password = Array(257).join('1');
+        const { error } = validate(user);
+        expect(error).not.toBeNull();
     });
 });
 
